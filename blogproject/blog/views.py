e80e27b19c3bf0039f5f245d1fab2eddef14ced4
Maxlevel <maxlevel1324@naver.com>
2019-06-28 10:57:37 +0900

    first commit

---
+++ blogproject/blog/views.py
@@ -0,0 +1,114 @@
+from django.shortcuts import render, get_object_or_404, redirect, HttpResponse
+from .models import Blog , Comment
+from django.utils import timezone
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+from .forms import BlogForm
+# Create your views here.
+
+def home(request):
+    blogs = Blog.objects
+    blog_list = Blog.objects.all()
+    paginator = Paginator(blog_list, 3)
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+    return render(request, 'home.html', {'blogs' :blogs, 'posts':posts})
+
+def details(request, blog_id):
+    details = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'details.html', {'detail': details})
+
+def new(request):
+    return render(request, 'new.html')
+
+def delete(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    blog.delete()
+    return redirect('/')
+
+def create(request):
+    # blog = Blog()
+    # blog.title = request.GET['title']
+    # blog.body = request.GET['body']
+    # blog.pub_date = timezone.datetime.now()
+    # blog.save()
+    # return redirect('/blog/' + str(blog.id))
+    if request.method == 'POST':
+        blog = Blog
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            BlogForm.pub_date = timezone.datetime.now()
+            bloggroup = form.save(commit=False)
+            bloggroup.save()
+            return redirect('/blog/' + str(blog.id))
+        else:
+            form = BlogForm()
+        return render(request, 'new.html', {'form' : form})
+
+
+def edit(request,blog_id):
+    # blog = get_object_or_404(Blog, pk=blog_id)
+    # if request.method == "POST":
+    #     blog.title = request.POST['title']
+    #     blog.body = request.POST['body']
+    #     blog.pub_date = timezone.datetime.now()
+    #     blog.save()
+    #     return redirect('/blog/' + str(blog.id))
+    # return render(request,'edit.html',{'blog':blog})
+    blog = get_object_or_404(Blog, pk=blog_id)
+    if request.method == 'POST':
+        form = BlogForm(request.POST, instance=blog)
+        if form.is_valid():
+            blog.pub_date = timezone.datetime.now()
+            blog = form.save(commit=False)
+            blog.save()
+            return redirect('details', pk=blog.id)
+    else:
+        form = BlogForm(instance=blog)
+    return render(request, 'edit.html', {'form': form, 'blog': blog})
+
+
+#blog/views.py
+
+@login_required
+def comment_add(request,blog_id):
+    if request.method == "POST":
+        post = Blog.objects.get(pk=blog_id)
+
+        comment = Comment()
+        comment.user = request.user
+        comment.body = request.POST['body']
+        comment.post = post
+        comment.save()
+        return redirect('/blog/' + str(blog_id))
+    else:
+        return HttpResponse('잘못된 접근입니다.')
+
+
+
+@login_required
+def comment_edit(request,comment_id):
+    comment = get_object_or_404(Comment,pk=comment_id)
+
+    if request.user == comment.user:
+        if request.method =="POST":
+            comment.body = request.POST['body']
+            comment.save()
+            return redirect('/blog/' + str(comment.post.id))
+        
+        elif request.method=="GET":
+            context ={
+                'comment' : comment
+            }
+            return render(request,'comment_edit.html', context)
+
+
+@login_required
+def comment_delete(request, comment_id):
+    comment = get_object_or_404(Comment,pk=comment_id)
+    if request.user == comment.user:
+        if request.mehtod=="POST":
+            post_id = comment.post.id
+            comment.delete()
+            return redirect('/blog/' + str(post_id))
+    return HttpResponse('잘못된 접근입니다.')
